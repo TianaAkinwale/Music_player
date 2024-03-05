3d7ba7c5d8c9939bb3c072673c1d25e02b79ab83
TianaAkinwale <tianorway@gmail.com>
2024-03-05 13:32:35 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    boxShadow: {
+      md: '0 0 4.5px -1px rgb(118, 118, 118)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
