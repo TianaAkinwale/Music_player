3eedb644ca6543dc1e9e3b84b013ab3b69cca00b
TianaAkinwale <tianorway@gmail.com>
2024-03-21 11:09:57 +0100

    ipdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-/* import { Routes, Route } from "react-router-dom";
- */
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
+	return (
+	<Routes>
+		<Route index element={<Login />} />
+		<Route path="login" element={<Login />} />
+    	</Routes>
+  );
+};
 
-  return (
-    {/* <Routes>
-      <Route index element={<Login />} />
-      <Route path="login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route
-        path="/Home"
-        element={<Home homePageElements={homePageElements} />}
-      />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/messages" element={<Messages />} />
-      <Route path="/LandingPage" element={<LandingPage />} />
-      <Route
-        path="/Notifications"
-        element={
-          <Notifications notificationsElements={notificationsElements} />
-        }
-      />
-      <Route path="/Settings" element={<Settings />} />
-      <Route path="*" element={<p>Page Not Found (Error 404!)</p>} />
-    </Routes> */}
-  )
-}
-
-export default App
+export default App;
